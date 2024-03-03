b546e69fc2453f3cb2abfcd8fc63e3521c18266d
root <root@ip-172-31-22-99.ap-southeast-1.compute.internal>
2024-03-03 15:39:37 +0800

    add staging connection

--- config/conn.php
+++ config/conn.php
@@ -1,13 +1,43 @@
 <?php
 
-$dbhost = "15.152.30.107";
-$dbuser = "ming";
-$dbpass = "Xiong1903@123";
-$dbname = "project1";
+// Default database connection settings for local environment
+$dbhost_local = "54.151.210.36";
+$dbuser_local = "ming";
+$dbpass_local = "Xiong1903@123";
+$dbname_local = "project1";
 
-if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
-{
+// Database connection settings for staging environment
+$dbhost_staging = "127.0.0.1";
+$dbuser_staging = "ming";
+$dbpass_staging = "Xiong1903@123";
+$dbname_staging = "project1";
 
-	die("failed to connect!");
+// Get the value of APPLICATION_ENV environment variable from Nginx
+$application_env = $_SERVER['APPLICATION_ENV'] ?? 'local';
+
+// Select database connection settings based on the environment
+if ($application_env === 'staging') {
+    $dbhost = $dbhost_staging;
+    $dbuser = $dbuser_staging;
+    $dbpass = $dbpass_staging;
+    $dbname = $dbname_staging;
+} else {
+    $dbhost = $dbhost_local;
+    $dbuser = $dbuser_local;
+    $dbpass = $dbpass_local;
+    $dbname = $dbname_local;
 }
-	
+
+// Connect to the database
+$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
+}
+
+// Close the connection
+mysqli_close($con);
+?>
