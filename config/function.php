7d262a5b518c80c2f2fbab16951d511d3b829eb1
shinla <winsun1127@gmail.com>
2024-03-05 12:39:49 +0800

    update

--- config/function.php
+++ config/function.php
@@ -20,41 +20,41 @@ function check_login($con)
 
 
 
-// function displayMiningTable($con) {
-//     // Fetch the latest 10 mining logs
-//     $miningLogsQuery = "SELECT start_time, vip_levels.level_name, vip_levels.rates, mining_logs.mining_amount
-//     FROM mining_logs
-//     INNER JOIN vip_levels ON mining_logs.user_id = vip_levels.vip_id
-//     ORDER BY start_time DESC
-//     LIMIT 10";
+function displayMiningTable($con) {
+    // Fetch the latest 10 mining logs
+    $miningLogsQuery = "SELECT start_time, vip_levels.level_name, vip_levels.rates, mining_logs.mining_amount
+    FROM mining_logs
+    INNER JOIN vip_levels ON mining_logs.user_id = vip_levels.vip_id
+    ORDER BY start_time DESC
+    LIMIT 10";
 
-//     $miningLogsResult = mysqli_query($con, $miningLogsQuery);
+    $miningLogsResult = mysqli_query($con, $miningLogsQuery);
 
-//     if ($miningLogsResult) {
-//         echo '<table border="1">
-//                 <thead>
-//                     <tr>
-//                         <th>Start Time</th>
-//                         <th>VIP Level</th>
-//                         <th>Rate</th>
-//                         <th>Amount</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>';
+    if ($miningLogsResult) {
+        echo '<table border="1">
+                <thead>
+                    <tr>
+                        <th>Start Time</th>
+                        <th>VIP Level</th>
+                        <th>Rate</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>';
         
-//         while ($row = mysqli_fetch_assoc($miningLogsResult)) {
-//             echo '<tr>';
-//             echo '<td>' . $row['start_time'] . '</td>';
-//             echo '<td>' . $row['level_name'] . '</td>';
-//             echo '<td>' . $row['rates'] . '</td>';
-//             echo '<td>' . $row['mining_amount'] . '</td>';
-//             echo '</tr>';
-//         }
+        while ($row = mysqli_fetch_assoc($miningLogsResult)) {
+            echo '<tr>';
+            echo '<td>' . $row['start_time'] . '</td>';
+            echo '<td>' . $row['level_name'] . '</td>';
+            echo '<td>' . $row['rates'] . '</td>';
+            echo '<td>' . $row['mining_amount'] . '</td>';
+            echo '</tr>';
+        }
 
-//         echo '</tbody></table>';
-//     } else {
-//         echo 'Error fetching mining logs: ' . mysqli_error($con);
-//     }
-// }
+        echo '</tbody></table>';
+    } else {
+        echo 'Error fetching mining logs: ' . mysqli_error($con);
+    }
+}
 
 ?>
